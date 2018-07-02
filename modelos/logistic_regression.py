c7c6ff3282d9fa36e4e393b43a5806f124027a2c
Fernando <f.rioja@alumnos.urjc.es>
2018-07-02 19:46:22 +0200

    Update file

--- modelos/logistic_regression.py
+++ modelos/logistic_regression.py
@@ -2,19 +2,22 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jun 16 17:59:02 2018
-
 @author: fernando
 """
 
+import itertools
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, average_precision_score, \
+                            f1_score, log_loss, precision_score, \
+                            recall_score, roc_auc_score, confusion_matrix
 
-data = open("Data.npy");
-output = open("Output.npy")
-X = np.load(data)
-Y = np.load(output)
+X = np.load("../Data.npy")
+Y = np.load("../Output.npy")
+X = np.delete(X, (380), axis=0)
+Y = np.delete(Y, (380), axis=0)
 
 model = LogisticRegression()
 X_train, X_test, y_train, y_test = \
@@ -25,6 +28,6 @@ X_train, X_test, y_train, y_test = \
 model.fit(X_train,y_train)
 print model
 
-y_pred_nb = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred_nb)
-print "accuracy_score: " + str(accuracy)
+#train and save this model
+y_lr = model.predict(X_test)
+np.save('y_lr.npy', y_lr)
